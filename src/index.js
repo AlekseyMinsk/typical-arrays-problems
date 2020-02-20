d54294315146909d9bee34ba2edabe2396dc1109
Aleksey Budanov <31888451+AlekseyMinsk@users.noreply.github.com>
2020-02-20 12:30:42 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+	if(array === undefined || array.length === 0) {
+		return 0;
+	}
+	var value = Math.min(...array);
+	return value;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+		return 0;
+	}
+	var value = Math.max(...array);
+	return value;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(array === undefined || array.length === 0) {
+		return 0;
+	}
+	var average = arr => arr.reduce((a,b) => a + b, 0) / arr.length;
+  return average(array);
 }
